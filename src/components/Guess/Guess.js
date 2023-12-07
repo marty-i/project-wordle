bea0b210941332c49c4e516d3e3925ac1f122eba
marty-i <bmn42+github@pm.me>
2023-12-07 16:19:36 +1000

    Exercise 3 handle undefined guesses method 2

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -4,21 +4,15 @@ import { range } from '../../utils';
 function Guess({ word }) {
   console.log({ word });
 
-  function createRow() {
-    if (word === undefined) {
-      return range(5).map((_, index) => (
-        <span key={index} className="cell"></span>
-      ));
-    }
-
-    return word.split('').map((letter, index) => (
-      <span key={index} className="cell">
-        {letter}
-      </span>
-    ));
-  }
-
-  return <p className="guess">{createRow()}</p>;
+  return (
+    <p className="guess">
+      {range(5).map((num) => (
+        <span key={num} className="cell">
+          {word ? word[num] : undefined}
+        </span>
+      ))}
+    </p>
+  );
 }
 
 export default Guess;
