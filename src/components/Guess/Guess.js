3f721eb8e37bb25557d17269e9f364982322c86b
marty-i <bmn42+github@pm.me>
2023-12-07 15:40:33 +1000

    Exercise 3 improved Guess using split

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,27 +3,15 @@ import React from 'react';
 function Guess({ word }) {
   console.log({ word });
 
-  function generateCellRow() {
-    let row = [];
-
-    if (word === undefined) {
-      return [
-        <span className="cell"></span>,
-        <span className="cell"></span>,
-        <span className="cell"></span>,
-        <span className="cell"></span>,
-        <span className="cell"></span>,
-      ];
-    }
-
-    for (let i = 0; i < word.name.length; i++) {
-      row.push(<span className="cell">{word.name[i]}</span>);
-    }
-
-    return row;
-  }
-
-  return <p className="guess">{generateCellRow()}</p>;
+  return (
+    <p className="guess">
+      {word.split('').map((letter, index) => (
+        <span key={index} className="cell">
+          {letter}
+        </span>
+      ))}
+    </p>
+  );
 }
 
 export default Guess;
